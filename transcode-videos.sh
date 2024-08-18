e35a5101a8e9e26d08ee369d4713341cbcf09992
Robert Adamis <104916889+atlasamerican@users.noreply.github.com>
2024-08-17 22:31:45 -0700

    Update transcode-videos.sh
    fixed typo

--- transcode-videos.sh
+++ transcode-videos.sh
@@ -73,7 +73,7 @@ prompt_user() {
     echo "Choose action for 360 files:"
     echo "  1. Change all media files container to .mov"
     echo "  2. Remap 360 files & change all media files container to .mov"
-    echo "  3. Remap 360 fioles only without changing the container"
+    echo "  3. Remap 360 files only without changing the container"
     read -r action
 
     if [ "$action" -eq 2 ] || [ "$action" -eq 3 ]; then
